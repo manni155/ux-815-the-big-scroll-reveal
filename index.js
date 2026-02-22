45314a32505245ad0885dfa286af3dc0979bec45
Thom Manning <84347286+manni155@users.noreply.github.com>
2026-02-22 07:04:03 +0000

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add JavaScript below
+ScrollReveal().reveal('body');
+
+ScrollReveal().reveal('.headline', { duration:2000 })
+
+ScrollReveal().reveal('#photo1', { 
+    duration:2000,
+    delay:500,
+    distance: '100px'
+ });
+
+ScrollReveal().reveal('#photo2', { 
+    duration:2500,
+    delay:750,
+    origin:top,
+ });
+
+ScrollReveal().reveal('#photo3', { 
+    duration:3500, 
+    opacity: 0.05,
+});
+
+ScrollReveal().reveal('#photo4', { 
+    duration:2500,
+    rotate: {x:20, z:20},
+});
+ScrollReveal().reveal('#photo5', { 
+    duration:2000,
+    rotate: {x:-20, z:-20}
+});
